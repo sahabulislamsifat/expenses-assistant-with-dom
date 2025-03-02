e221509b7472343c746787fd17d5c67765f98d5f
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-02 16:00:07 +0600

    addEventListener for calculate Savings

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // console.log("connect....");
 
+// Getting all the values
+// const income = Number(document.getElementById("income").value);
+// const software = Number(document.getElementById("software").value);
+// const courses = Number(document.getElementById("courses").value);
+// const internet = Number(document.getElementById("internet").value);
+
 // Add Event Listener for Calculate Button
 
 const calculate = document.getElementById("calculate");
@@ -11,7 +17,6 @@ calculate.addEventListener("click", function () {
   const software = Number(document.getElementById("software").value);
   const courses = Number(document.getElementById("courses").value);
   const internet = Number(document.getElementById("internet").value);
-
   //   console.log(income, software, courses, internet);
   const totalExpense = software + courses + internet;
   const balance = income - totalExpense;
@@ -25,4 +30,29 @@ calculate.addEventListener("click", function () {
 
   const resultVisible = document.getElementById("results");
   resultVisible.classList.remove("hidden");
+});
+
+// addEventListener for Savings button
+
+const calculateSavings = document.getElementById("calculate-savings");
+
+calculateSavings.addEventListener("click", function () {
+  //   console.log("testing......");
+  const income = Number(document.getElementById("income").value);
+  const software = Number(document.getElementById("software").value);
+  const courses = Number(document.getElementById("courses").value);
+  const internet = Number(document.getElementById("internet").value);
+  const savingsPercentage = Number(document.getElementById("savings").value);
+  // console.log(savingsPercentage);
+  const totalExpense = software + courses + internet;
+  const balance = income - totalExpense;
+
+  const savingAmount = (savingsPercentage * balance) / 100;
+  const remainingBalance = balance - savingAmount;
+  //   console.log(savingAmount);
+  const savingsElement = document.getElementById("savings-amount");
+  savingsElement.innerText = savingAmount.toFixed(2);
+
+  const remainingBalanceElement = document.getElementById("remaining-balance");
+  remainingBalanceElement.innerText = remainingBalance.toFixed(2);
 });
